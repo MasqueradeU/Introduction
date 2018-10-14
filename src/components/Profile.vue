7cb20555eb16222a41f235580ba5ca24d3a58134
Hitomi Hatsukaze <gigigiaru776@yahoo.co.jp>
2018-10-14 23:18:13 +0900

    Modify Profile layout

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,24 +1,29 @@
 <template>
-  <div class="hello">
-    <img src="../assets/profile_picture.png" id="profile-picture"/><br>
-    <a href="https://github.com/" target="_blank">
-      <img src="../assets/github.svg" width="16" height="16">
-    </a>
-    <a href="https://twitter.com/htmhtkz" target="_blank">
-      <img src="../assets/twitter.svg" width="16" height="16" id="twitter-icon">
-    </a>
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
+  <div class="profile">
+    <div id="profile0">
+      <img src="../assets/profile_picture.png" class="profile-picture"/>
+      <h2>{{ name }}
+        <a href="https://github.com/MasqueradeU/introduction" target="_blank">
+          <img src="../assets/github.svg" width="16" height="16">
         </a>
-      </li>
-    </ul>
+        <a href="https://twitter.com/htmhtkz" target="_blank">
+          <img src="../assets/twitter.svg" width="16" height="16">
+        </a>
+      </h2>
+      <p>{{ description }}</p>
+      <p>
+        <img src="../assets/globe.svg" width="16" height="16">{{ prefecture }}
+      </p>
+      <p>
+        <img src="../assets/thumbtack.svg" width="16" height="16">{{ currentPrefecture }}
+      </p>
+      <p>
+        <img src="../assets/link.svg" width="16" height="16">
+        <a href="https://twitter.com/htmhtkz" target="_blank">
+          {{ ULR1 }}
+        </a>
+      </p>
+    </div>
   </div>
 </template>
 
@@ -27,29 +32,47 @@ export default {
   name: 'Profile',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      name: 'Hitomi Hatsukaze',
+      description: 'A web developer in tokyo prefecture. JS, Laravel and C++(programming competition)',
+      prefecture: 'Aichi',
+      currentPrefecture: 'Tokyo',
+      ULR1: 'H.Hkaze'
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#Profile-picture {
-  width: 256px;
+.profile {
+  max-width: 1060px;
+  display: flex;
+}
+
+#profile0{
+  position: relative;
+  max-width: 25%;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid rgba(0,0,0,.125);
+  margin-left: 10px
+}
+
+.profile-picture {
   height: 256px;
 }
+
 h1, h2 {
   font-weight: normal;
+  margin: 10px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+p {
+  font-weight: normal;
+  display: block;
+  font-size: 120%;
+  margin: 10px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+
 a {
   color: #42b983;
 }
