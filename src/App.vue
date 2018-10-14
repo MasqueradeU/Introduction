79354c95f1b28a4b04f98dc4b7d96f3358af451b
Hitomi Hatsukaze <gigigiaru776@yahoo.co.jp>
2018-10-14 21:56:35 +0900

    Modify App

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <Profile/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Profile from './components/Profile'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Profile
   }
 }
 </script>
@@ -21,7 +20,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
 }
